4a4f264d7a3ea6ae044a025217a429a2529eadb0
arushiahmed <aaahmed@umass.edu>
2019-12-18 11:40:19 -0500

    Updated About.jsx
    Added details from the About.md file

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -20,7 +20,7 @@ export default class SearchBar extends React.Component {
                     <button className="btn btn-dark"><Link to="/connect" style={{ textDecoration: 'none', color: 'white' }}>Connect With Students</Link></button>
                     <button className="btn btn-dark"><Link to="/userSurvey" style={{ textDecoration: 'none', color: 'white' }}>Survery</Link></button>
                     <button className="btn btn-dark"><Link to="/profile" style={{ textDecoration: 'none', color: 'white' }}>Profile</Link></button>
-                    <button className="btn btn-dark"><Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Sign Out</Link></button>
+                    <button className="btn btn-dark"><Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Logout</Link></button>
                     <form className="form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
--- static/app.bundle.js
+++ static/app.bundle.js
@@ -805,6 +805,13 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var textStyle = {
+  marginLeft: '15%',
+  marginRight: '15%',
+  // width: '850px',
+  fontSize: '14px'
+};
+
 var About = function (_React$Component) {
   _inherits(About, _React$Component);
 
@@ -832,14 +839,80 @@ var About = function (_React$Component) {
           'center',
           null,
           _react2.default.createElement(
-            'p',
-            null,
-            'Welcome to Intermingle.'
-          ),
-          _react2.default.createElement(
-            'p',
-            null,
-            'This is an interactive platform where international students and domestic students can find friends. Majority of this website is not implemented.'
+            'div',
+            { className: 'about', style: textStyle },
+            _react2.default.createElement(
+              'h3',
+              null,
+              'Welcome to Intermingle.'
+            ),
+            _react2.default.createElement('br', null),
+            _react2.default.createElement(
+              'p',
+              null,
+              'This is an interactive platform where international students and domestic students can find friends. Majority of this website is not implemented. Below is a list of the pages of the site, how a user would interact with the page, and what needs to be implemented on the page.'
+            ),
+            _react2.default.createElement('br', null),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '1. Welcome: '
+              ),
+              ' This is the home page where users either signin and signup. If the user is signing up then they would fill out the form that will directly take them to the survey once completed. The signin page is for returning users where they fill in their username (set to their email) and the password where they will be directed to the about page.'
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '2. About: '
+              ),
+              ' The about page will give them the infomation about the application. The about page will have contain content from the About.md file as well.'
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '3. Connect With Friends: '
+              ),
+              ' This page is where users will look for friends. Each person has their own card that gives the user the name, if they are a undergraduate or graduate student, school, the year based on graduation, their overall rating, and the bio that describes who they are. This page also has two filters implemented. One filter is where users can look for either undergraduates or graduate students and the second filter is if the user is trying to find friends from a certain rating. If the user likes a person, then they can click on their card that will direct them to their profile to find the chat button to start a conversation with them. Their is also a rating button where the user can go rate the person and also see other reviews about how others describe this person. Currently the list of friends is mock data running from mongo scripts/init.mongo.js.'
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '4. Survey: '
+              ),
+              'This page is where the user will see their survey. Currently this page show the survey sections and questions with no data that is fill. To fill out the survey the user will click on the new survey button and bring them to a new survey page. The user will fill the form then press the save button then next click on the find friends button that will give them a list of friends that would be a good match for them based on the survey. Currenlty the list of friends is just mock data that does not filiter it to their survey. When the user presses the save button, they can go back to the survey button and scrool down and see the new updated survey they have filled out.'
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '5. Profile: '
+              ),
+              'This is the user profile where they can see thier email, wether they are a undergraduate or graduate student, the school, which country they are from, and the bio they have written about themseleves. There is an edit button when the user wants to change their profile, a chat button where the user can go talk to their friends, and a rating button to see their reviews. Currently none of the buttons on the page work.'
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              _react2.default.createElement(
+                'strong',
+                null,
+                '6. Logout Button:'
+              ),
+              ' This is a button not a page. When the user clicks on this button, it will direct them back to the welcome page.'
+            )
           )
         )
       );
@@ -3057,7 +3130,7 @@ var SearchBar = function (_React$Component) {
               _react2.default.createElement(
                 _reactRouter.Link,
                 { to: '/', style: { textDecoration: 'none', color: 'white' } },
-                'Sign Out'
+                'Logout'
               )
             ),
             _react2.default.createElement(
